569a487f46aa1fbb9119e37a173f92a87a1adc91
hoang bao <baohoang.7882@hotmail.com>
2023-09-28 10:11:43 +0700

    feat: add base exception handler

---
+++ API_v1/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase {
+        private readonly ILogger _logger;
+
+        public AuthController(ILogger<AuthController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get() {
+            throw new Exception("Error when login");
+            return Ok(new {Email = "asdasd", Password = "asdasd" });
+        }
+    }
+}
--- API_v1/Program.cs
+++ API_v1/Program.cs
@@ -1,3 +1,5 @@
+using API.ErrorHandling;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,12 +10,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.ConfigureExceptionHandler(logger);
 
 app.UseHttpsRedirection();
 
