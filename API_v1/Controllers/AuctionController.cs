03e36afe65663ab1262ee8ca3e432d9ae056d8fa
DucLM42 <leduchien09@gmail.com>
2023-10-19 13:51:08 +0700

    fix: checkregister role seller

--- API_v1/Controllers/AuctionController.cs
+++ API_v1/Controllers/AuctionController.cs
@@ -381,7 +381,7 @@ namespace API.Controllers
         {
             var userId = GetUserIdFromToken();
             var user = _userService.Get(userId);
-            if (user == null || user.Role != (int)Role.Buyer)
+            if (user == null)
             {
                 return Unauthorized(new ErrorDetails
                 {
