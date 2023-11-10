11f5cadd61afa698c25c85ed0ed22a18c0577d70
DucLM42 <leduchien09@gmail.com>
2023-11-10 13:35:46 +0700

    fix: mvc view namespace and order cancel request approve

--- API_v1/Controllers/OrderController.cs
+++ API_v1/Controllers/OrderController.cs
@@ -24,13 +24,15 @@ namespace API.Controllers
     [ApiController]
     [Authorize]
     [EnableCors]
-    public class OrderController : ControllerBase {
+    public class OrderController : ControllerBase
+    {
         private readonly IOrderService _orderService;
         private readonly IUserService _userService;
         private readonly IEmailService _emailService;
         private readonly IMapper _mapper;
 
-        public OrderController(IOrderService orderService, IUserService userService, IMapper mapper, IEmailService emailService) {
+        public OrderController(IOrderService orderService, IUserService userService, IMapper mapper, IEmailService emailService)
+        {
             _orderService = orderService;
             _userService = userService;
             _mapper = mapper;
@@ -38,34 +40,43 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get([FromQuery] PagingParam pagingParam, [FromQuery] int Status) {
+        public IActionResult Get([FromQuery] PagingParam pagingParam, [FromQuery] int Status)
+        {
             var list = _orderService.GetAll(Status);
-            return Ok(new BaseResponse {
-                Code = (int) HttpStatusCode.OK,
+            return Ok(new BaseResponse
+            {
+                Code = (int)HttpStatusCode.OK,
                 Message = "Get order list successfully",
-                Data = new {
+                Data = new
+                {
                     Count = list.Count,
                     List = list.Skip((pagingParam.PageNumber - 1) * pagingParam.PageSize).Take(pagingParam.PageSize).ToList()
                             .Select(p => _mapper.Map<OrderResponse>(p))
                 }
             });
         }
-        
+
         [HttpGet("{orderId:int}")]
-        public IActionResult Get(int orderId) {
+        public IActionResult Get(int orderId)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { 
-                    StatusCode = (int) HttpStatusCode.Unauthorized, 
-                    Message = "You are not logged in to access this" });
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails
+                {
+                    StatusCode = (int)HttpStatusCode.Unauthorized,
+                    Message = "You are not logged in to access this"
+                });
             }
 
             var order = _orderService.Get(orderId);
-            if (order == null) {
-                return NotFound(new ErrorDetails {
-                    StatusCode = (int) HttpStatusCode.NotFound,
+            if (order == null)
+            {
+                return NotFound(new ErrorDetails
+                {
+                    StatusCode = (int)HttpStatusCode.NotFound,
                     Message = "Order not found"
                 });
             }
@@ -75,138 +86,156 @@ namespace API.Controllers
             //        Message = "You are not allowed to access this order"
             //    });
             //}
-            return Ok(new BaseResponse { 
-                Code = (int) HttpStatusCode.OK, 
+            return Ok(new BaseResponse
+            {
+                Code = (int)HttpStatusCode.OK,
                 Message = "Get order successfully",
-                Data = _mapper.Map<OrderResponse>(order) 
+                Data = _mapper.Map<OrderResponse>(order)
             });
         }
 
         [HttpGet("/api/buyer/order")]
-        public IActionResult GetByBuyerId([FromQuery] PagingParam pagingParam, [FromQuery] OrderParam orderParam) {
+        public IActionResult GetByBuyerId([FromQuery] PagingParam pagingParam, [FromQuery] OrderParam orderParam)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
 
             var orders = _orderService.GetByBuyerId(id, orderParam.Status)
                 .Skip((pagingParam.PageNumber - 1) * pagingParam.PageSize).Take(pagingParam.PageSize).ToList();
             var responseOrders = orders.Select(p => _mapper.Map<OrderResponse>(p));
-            return Ok(new BaseResponse { Code = (int) HttpStatusCode.OK, Message = "Get Buyer's order list successfully", Data = responseOrders});
+            return Ok(new BaseResponse { Code = (int)HttpStatusCode.OK, Message = "Get Buyer's order list successfully", Data = responseOrders });
         }
-        
+
         [HttpGet("/api/seller/order")]
-        public IActionResult GetBySellerId([FromQuery] PagingParam pagingParam, [FromQuery] OrderParam orderParam) {
+        public IActionResult GetBySellerId([FromQuery] PagingParam pagingParam, [FromQuery] OrderParam orderParam)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
 
             var orders = _orderService.GetBySellerId(id, orderParam.Status)
                 .Skip((pagingParam.PageNumber - 1) * pagingParam.PageSize).Take(pagingParam.PageSize).ToList();
             var responseOrders = orders.Select(p => _mapper.Map<OrderResponse>(p));
-            return Ok(new BaseResponse { Code = (int) HttpStatusCode.OK, Message = "Get Seller's order list successfully", Data = responseOrders});
+            return Ok(new BaseResponse { Code = (int)HttpStatusCode.OK, Message = "Get Seller's order list successfully", Data = responseOrders });
         }
 
         [HttpPost("/api/buyer/checkout")]
         [ServiceFilter(typeof(ValidateModelAttribute))]
-        public IActionResult Checkout([FromBody] CheckoutRequest request) {
+        public IActionResult Checkout([FromBody] CheckoutRequest request)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { 
-                    StatusCode = (int) HttpStatusCode.Unauthorized, 
-                    Message = "You are not allowed to access this" 
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails
+                {
+                    StatusCode = (int)HttpStatusCode.Unauthorized,
+                    Message = "You are not allowed to access this"
                 });
             }
-            
-            if (user.Role != (int) Role.Buyer && user.Role != (int) Role.Seller) {
-                return Unauthorized(new ErrorDetails { 
-                    StatusCode = (int) HttpStatusCode.Unauthorized, 
-                    Message = "You are not allowed to access this" 
+
+            if (user.Role != (int)Role.Buyer && user.Role != (int)Role.Seller)
+            {
+                return Unauthorized(new ErrorDetails
+                {
+                    StatusCode = (int)HttpStatusCode.Unauthorized,
+                    Message = "You are not allowed to access this"
                 });
             }
             _orderService.Checkout(request, user.Id, request.AddressId);
-            return Ok(new BaseResponse { Code = (int) HttpStatusCode.OK, Message = "Checkout successfully", Data = null });
+            return Ok(new BaseResponse { Code = (int)HttpStatusCode.OK, Message = "Checkout successfully", Data = null });
         }
 
-        private int GetUserIdFromToken() {
+        private int GetUserIdFromToken()
+        {
             var user = HttpContext.User;
             return int.Parse(user.Claims.FirstOrDefault(p => p.Type == "UserId").Value);
         }
 
-        [HttpPut("/api/seller/order/cancelrequest/{orderId:int}")]
-        public async Task<IActionResult> ApproveCancelRequestAsync(int orderId, [FromBody] bool confirm) {
+        [HttpPost("/api/seller/order/cancelrequest/{orderId:int}")]
+        public async Task<IActionResult> ApproveCancelRequestAsync(int orderId)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
-            
-            if (user.Role != (int) Role.Seller) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+
+            if (user.Role != (int)Role.Seller)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
-            if (confirm) {
-                _orderService.ApproveCancelOrderRequest(id, orderId);
-                var message = new Message(new string[] { user.Email }, $"Order #{orderId} updates", $"Your order cancellation has been approved");
-                await _emailService.SendEmail(message);
-                return Ok(new BaseResponse { Code = (int) HttpStatusCode.OK, Message = "Approve cancel request successfully", Data = null });
-            }
-            //implement deny flow
-            return BadRequest(new ErrorDetails { StatusCode = (int) HttpStatusCode.InternalServerError, Message = "Not implemented" });
+            _orderService.ApproveCancelOrderRequest(id, orderId);
+            var message = new Message(new string[] { user.Email }, $"Order #{orderId} updates", $"Your order cancellation has been approved");
+            await _emailService.SendEmail(message);
+            return Ok(new BaseResponse { Code = (int)HttpStatusCode.OK, Message = "Approve cancel request successfully", Data = null });
         }
-        
+
         [HttpPost("/api/buyer/order/cancelrequest/{orderId:int}")]
-        public IActionResult CreateCancelRequest(int orderId) {
+        public IActionResult CreateCancelRequest(int orderId)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
-            
-            if (user.Role != (int) Role.Buyer && user.Role != (int) Role.Seller) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+
+            if (user.Role != (int)Role.Buyer && user.Role != (int)Role.Seller)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
 
             _orderService.CancelOrderRequest(id, orderId);
-            return Ok(new BaseResponse { Code = (int) HttpStatusCode.OK, Message = "Create cancel request successfully", Data = null });
+            return Ok(new BaseResponse { Code = (int)HttpStatusCode.OK, Message = "Create cancel request successfully", Data = null });
         }
-        
+
         [HttpPost("/api/seller/order/{orderId:int}")]
-        public async Task<IActionResult> ConfirmOrderAsync(int orderId, [FromBody] ConfirmOrderRequest request) {
+        public async Task<IActionResult> ConfirmOrderAsync(int orderId, [FromBody] ConfirmOrderRequest request)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
-            
-            if (user.Role != (int) Role.Seller) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not seller, not allowed to access this" });
+
+            if (user.Role != (int)Role.Seller)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not seller, not allowed to access this" });
             }
-            
-            if (request.Status == (int)OrderStatus.CancelledBySeller) {
+
+            if (request.Status == (int)OrderStatus.CancelledBySeller)
+            {
                 _orderService.SellerCancelOrder(id, orderId, request.message);
                 var message = new Message(new string[] { user.Email }, $"Order #{orderId} updates", $"Your order has been cancelled by seller\nReason: {request.message}");
                 await _emailService.SendEmail(message);
-                return Ok(new BaseResponse { Code = (int) HttpStatusCode.OK, Message = "Cancel order successfully", Data = null });
+                return Ok(new BaseResponse { Code = (int)HttpStatusCode.OK, Message = "Cancel order successfully", Data = null });
             }
 
-            if (request.Status == (int) OrderStatus.ReadyForPickup) {
+            if (request.Status == (int)OrderStatus.ReadyForPickup)
+            {
                 _orderService.UpdateOrderStatus(id, request.Status, orderId);
                 await _orderService.CreateShippingOrder(orderId);
                 var message = new Message(new string[] { user.Email }, $"Order #{orderId} updates", $"Your order has been accepted by seller");
                 await _emailService.SendEmail(message);
-                return Ok(new BaseResponse { Code = (int) HttpStatusCode.OK, Message = "Confirm order successfully", Data = null });
+                return Ok(new BaseResponse { Code = (int)HttpStatusCode.OK, Message = "Confirm order successfully", Data = null });
             }
-            return BadRequest(new ErrorDetails { StatusCode = (int) HttpStatusCode.BadRequest, Message = "Status not matching required information"});
+            return BadRequest(new ErrorDetails { StatusCode = (int)HttpStatusCode.BadRequest, Message = "Status not matching required information" });
         }
 
         //[HttpPost("/api/seller/order/deliver/{orderId:int}")]
@@ -230,40 +259,50 @@ namespace API.Controllers
         //}
 
         [HttpPost("/api/buyer/order/done/{orderId:int}")]
-        public IActionResult BuyerConfirmDoneOrder(int orderId) {
+        public IActionResult BuyerConfirmDoneOrder(int orderId)
+        {
             var id = GetUserIdFromToken();
             var user = _userService.Get(id);
 
-            if (user == null) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
+            if (user == null)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not allowed to access this" });
             }
 
-            if (user.Role != (int) Role.Seller && user.Role != (int) Role.Buyer) {
-                return Unauthorized(new ErrorDetails { StatusCode = (int) HttpStatusCode.Unauthorized, Message = "You are not buyer, not allowed to access this" });
+            if (user.Role != (int)Role.Seller && user.Role != (int)Role.Buyer)
+            {
+                return Unauthorized(new ErrorDetails { StatusCode = (int)HttpStatusCode.Unauthorized, Message = "You are not buyer, not allowed to access this" });
             }
 
             var order = _orderService.Get(orderId);
-            if (order == null) {
-                return NotFound(new ErrorDetails {
-                    StatusCode = (int) HttpStatusCode.NotFound,
+            if (order == null)
+            {
+                return NotFound(new ErrorDetails
+                {
+                    StatusCode = (int)HttpStatusCode.NotFound,
                     Message = "Order not found"
                 });
             }
-            if (order.BuyerId != id) {
-                return Unauthorized(new ErrorDetails {
-                    StatusCode = (int) HttpStatusCode.Unauthorized,
+            if (order.BuyerId != id)
+            {
+                return Unauthorized(new ErrorDetails
+                {
+                    StatusCode = (int)HttpStatusCode.Unauthorized,
                     Message = "You are not allowed to done this order"
                 });
             }
-            if (order.Status != (int) OrderStatus.Delivered) {
-                return Unauthorized(new ErrorDetails {
-                    StatusCode = (int) HttpStatusCode.Unauthorized,
+            if (order.Status != (int)OrderStatus.Delivered)
+            {
+                return Unauthorized(new ErrorDetails
+                {
+                    StatusCode = (int)HttpStatusCode.Unauthorized,
                     Message = "You are not allowed to done this order"
                 });
             }
             _orderService.DoneOrder(orderId);
-            return Ok(new BaseResponse {
-                Code = (int) HttpStatusCode.OK,
+            return Ok(new BaseResponse
+            {
+                Code = (int)HttpStatusCode.OK,
                 Message = "Done order successfully",
                 Data = null
             });
--- API_v1/Views/Mvc/Error.cshtml.cs
+++ API_v1/Views/Mvc/Error.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace API.View.Mvc
+namespace API.Views.Mvc
 {
     public class ErrorModel : PageModel
     {
