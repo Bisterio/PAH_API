fe0b13a3823d59d5d1219a86ab43b31c6e211ed0
DucLM42 <leduchien09@gmail.com>
2023-11-10 14:00:15 +0700

    fix: admin get ended auctions

--- API_v1/Controllers/AuctionController.cs
+++ API_v1/Controllers/AuctionController.cs
@@ -492,7 +492,7 @@ namespace API.Controllers
         {
             var userId = GetUserIdFromToken();
             var user = _userService.Get(userId);
-            if (user == null || user.Role != (int)Role.Manager)
+            if (user == null || (user.Role != (int)Role.Manager && user.Role != (int)Role.Administrator))
             {
                 return Unauthorized(new ErrorDetails
                 {
