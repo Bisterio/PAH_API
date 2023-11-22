9147cf49ee2599d3c7d78e33f930cc28fa8742e5
Bisterio <ngocchau1103@gmail.com>
2023-11-22 14:35:04 +0700

    add error message for validation in profile request

--- Request/UpdateProfileRequest.cs
+++ Request/UpdateProfileRequest.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Request {
     public class UpdateProfileRequest : IValidatableObject{
-        [Required]
+        [Required(ErrorMessage = "Không được để trống tên")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -15,26 +15,26 @@ namespace Request {
         //[DataType(DataType.Password)]
         //public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Không được để trống số điện thoại")]
         [Phone]
         public string Phone { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Không được để trống ảnh đại diện")]
         [Url]
         public string ProfilePicture { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Không được để trống giới tính")]
         [Range(0, 1)]
         public int Gender { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Không được để trống ngày sinh")]
         public DateTime Dob { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
             int age = DateTime.Now.Year - Dob.Year;
             if (DateTime.Now.DayOfYear < Dob.DayOfYear) age = age - 1;
             if (age < 18) {
-                yield return new ValidationResult($"Date of birth must be 18 years or older", new[] { nameof(Dob) });
+                yield return new ValidationResult($"Người dùng phải hơn 18 tuổi", new[] { nameof(Dob) });
             }
         }
     }
